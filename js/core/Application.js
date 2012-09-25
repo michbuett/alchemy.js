fe9b9efdfe28edacc42e6b65b276ab3a84604e30
Michael Buettner <mbuettner@seitenbau.net>
2012-09-25 19:25:11 +0200

    Port application prototype to new type structure

--- js/core/Application.js
+++ js/core/Application.js
@@ -1,113 +1,158 @@
-Alchemy.ns('Alchemy.app');
-
-/**
- * @class Alchemy.Application
- * @extends Alchemy.MateriaPrima
- * @singleton
+/*
+ * Copyright (C) 2012 Michael Büttner
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+ * the Software.
+ *
+ * The Software shall not be used for discriminating or manipulating people.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
-Alchemy.Application = (function () {
-    var paused = false;
-    var stopped = true;
 
-    return {
-        /**
-         * @property fps
-         * @type Alchemy.util.MeanValue
-         */
-        fps: undefined,
+(function () {
+    'use strict';
 
-        prepare: Alchemy.emptyFn,
+    var alchemy = require('./alchemy.js');
 
-        update: Alchemy.emptyFn,
+    /**
+     * Description
+     *
+     * @class core.Application
+     * @extends core.MateriaPrima
+     */
+    alchemy.addFormula({
+        name: 'core.Application',
+        extend: 'core.MateriaPrima',
+        overrides: {
+            /**
+             * <code>true</code> if the app has been stopped (not running)
+             *
+             * @property stopped
+             * @type Boolean
+             * @private
+             */
+            stopped: true,
 
-        draw: Alchemy.emptyFn,
+            /**
+             * <code>true</code if the app has been paused
+             *
+             * @property paused
+             * @type Boolean
+             * @private
+             */
+            paused: false,
 
-        finish: Alchemy.emptyFn,
+            /**
+             * The frames per second
+             *
+             * @property fps
+             * @type util.MeanValue
+             */
+            fps: undefined,
 
-        /**
-         * starts the application loop;
-         * this will call the {@link #prepare} method
-         */
-        start: function () {
-            if (stopped) {
-                stopped = false;
-                this.frame = 0;
-                this.boundLoopFn = this.boundLoopFn || this.loop.bind(this);
-                this.fps = Alchemy.util.MeanValue.create(20);
-                this.prepare();
+            init: function () {
+                this.fps = alchemy('util.MeanValue').create(20);
+                this.boundLoopFn = this.loop.bind(this);
+            },
 
-                this.lastTick = Date.now();
-                Alchemy.reqAnimFrame(this.boundLoopFn);
-            }
-        },
+            prepare: alchemy.emptyFn,
 
-        /**
-         * stops the application loop;
-         * this will call the {@link #finish} method
-         */
-        end: function () {
-            if (!stopped) {
-                this.finish();
-                stopped = true;
-            }
-        },
+            update: alchemy.emptyFn,
 
-        /**
-         * the loop method; called every iteration;
-         * will call the {@link #update} and the {@link #draw} method
-         * @private
-         */
-        loop: function () {
-            if (!stopped) {
-                var currentTick = Date.now();
-                var diff = (currentTick - this.lastTick) || 1;
-                this.lastTick = currentTick;
-                this.fps.add(1000 / diff);
+            draw: alchemy.emptyFn,
 
-                Alchemy.reqAnimFrame(this.boundLoopFn);
-                if (!paused) {
-                    this.update(this.frame);
-                    this.draw(this.frame);
-                    this.frame++;
+            finish: alchemy.emptyFn,
+
+            /**
+             * starts the application loop;
+             * this will call the {@link #prepare} method
+             */
+            start: function () {
+                if (this.stopped) {
+                    this.stopped = false;
+                    this.frame = 0;
+                    this.prepare();
+                    this.lastTick = Date.now();
+
+                    alchemy.reqAnimFrame(this.boundLoopFn);
                 }
+            },
+
+            /**
+             * stops the application loop;
+             * this will call the {@link #finish} method
+             */
+            end: function () {
+                if (!this.stopped) {
+                    this.finish();
+                    this.stopped = true;
+                }
+            },
+
+            /**
+             * the loop method; called every iteration;
+             * will call the {@link #update} and the {@link #draw} method
+             * @private
+             */
+            loop: function () {
+                if (!this.stopped) {
+                    var currentTick = Date.now();
+                    var diff = (currentTick - this.lastTick) || 1;
+
+                    this.lastTick = currentTick;
+                    this.fps.add(1000 / diff);
+
+                    alchemy.reqAnimFrame(this.boundLoopFn);
+                    if (!this.paused) {
+                        this.update(this.frame);
+                        this.draw(this.frame);
+                        this.frame++;
+                    }
+                }
+            },
+
+            /**
+             * returns <code>true</code> if and only if the current application
+             * is running (it may or may not be paused though)
+             *
+             * @return Boolean
+             */
+            isRunning: function () {
+                return !this.stopped;
+            },
+
+            /**
+             * pauses the application
+             */
+            pause: function () {
+                this.paused = true;
+            },
+
+            /**
+             * unpauses the application
+             */
+            unpause: function () {
+                this.paused = false;
+            },
+
+            /**
+             * returns <code>true</code> if the application is paused
+             *
+             * @return {Boolean}
+             */
+            isPaused: function () {
+                return this.paused;
             }
-        },
-
-        /**
-         * returns <code>true</code> if and only if the current application
-         * is running (it may or may not be paused though)
-         *
-         * @return Boolean
-         */
-        isRunning: function () {
-            return !stopped;
-        },
-
-        /**
-         * pauses the application
-         */
-        pause: function () {
-            paused = true;
-        },
-
-        /**
-         * unpauses the application
-         */
-        unpause: function () {
-            paused = false;
-        },
-
-        /**
-         * returns <code>true</code> if the application is paused
-         *
-         * @return {Boolean}
-         */
-        isPaused: function () {
-            return paused;
         }
-    };
-})();
+    });
+}());
 
-Alchemy.Application = Alchemy.brew({
-    extend: Alchemy.MateriaPrima
-}, Alchemy.Application);
