744496cf1109835c9567753c4e26419ec7448b81
Michael Buettner <mbuettner@seitenbau.net>
2012-10-28 13:55:12 +0100

    Improve and test removing of event listners

--- specs/occulus.spec.js
+++ specs/occulus.spec.js
@@ -12,7 +12,7 @@ describe('core.Oculus', function () {
     });
 
     describe('Listeners and event trigger', function () {
-        it('allows to add listner', function () {
+        it('allows to add listener', function () {
             // prepare
             var spy = jasmine.createSpy('test');
             var data = {foo: 'bar'};
@@ -24,7 +24,7 @@ describe('core.Oculus', function () {
             expect(spy.mostRecentCall.args[0]).toBe(data);
         });
 
-        it('allows to add mutiple listners for the same event', function () {
+        it('allows to add mutiple listeners for the same event', function () {
             // prepare
             var spy1 = jasmine.createSpy('listener 1');
             var spy2 = jasmine.createSpy('listener 2');
@@ -52,6 +52,98 @@ describe('core.Oculus', function () {
             this.oculus.trigger('myEvent');
             // verify
             expect(actualScope).toBe(expectedScope);
+        });
+
+    });
+
+    describe('removing listeners', function () {
+        var specialEvent = 'specialEvent';
+        var events = [specialEvent, 'event2', 'event3'];
+        var specialScope = {};
+        var listeners = [{
+            event: specialEvent,
+            fn: jasmine.createSpy('listener1'),
+            scope: specialScope
+        }, {
+            event: specialEvent,
+            fn: jasmine.createSpy('listener2'),
+            scope: {}
+        }, {
+            event: events[1],
+            fn: jasmine.createSpy('listener3'),
+            scope: specialScope
+        }, {
+            event: events[2],
+            fn: jasmine.createSpy('listener4'),
+            scope: {}
+        }];
+
+        beforeEach(function () {
+            listeners.forEach(function (l) {
+                l.fn.reset();
+                this.oculus.on(l.event, l.fn, l.scope);
+            }, this);
+        });
+
+        it('can remove a single listener', function () {
+            // prepare
+            this.oculus.off(listeners[0].event, listeners[0].fn, listeners[0].scope);
+            // execute
+            events.forEach(this.oculus.trigger, this.oculus);
+            // verify
+            expect(listeners[0].fn).not.toHaveBeenCalled();
+            expect(listeners[1].fn).toHaveBeenCalled();
+            expect(listeners[2].fn).toHaveBeenCalled();
+            expect(listeners[3].fn).toHaveBeenCalled();
+        });
+
+        it('can remove all listeners for an event', function () {
+            // prepare
+            this.oculus.off(specialEvent);
+            // execute
+            events.forEach(this.oculus.trigger, this.oculus);
+            // verify
+            expect(listeners[0].fn).not.toHaveBeenCalled();
+            expect(listeners[1].fn).not.toHaveBeenCalled();
+            expect(listeners[2].fn).toHaveBeenCalled();
+            expect(listeners[3].fn).toHaveBeenCalled();
+        });
+
+        it('can remove all listeners for a handler function', function () {
+            // prepare
+            this.oculus.off(null, listeners[1].fn);
+            // execute
+            events.forEach(this.oculus.trigger, this.oculus);
+            // verify
+            expect(listeners[0].fn).toHaveBeenCalled();
+            expect(listeners[1].fn).not.toHaveBeenCalled();
+            expect(listeners[2].fn).toHaveBeenCalled();
+            expect(listeners[3].fn).toHaveBeenCalled();
+        });
+
+        it('can remove all listeners for given scope', function () {
+            // prepare
+            this.oculus.off(null, null, specialScope);
+            // execute
+            events.forEach(this.oculus.trigger, this.oculus);
+            // verify
+            expect(listeners[0].fn).not.toHaveBeenCalled();
+            expect(listeners[1].fn).toHaveBeenCalled();
+            expect(listeners[2].fn).not.toHaveBeenCalled();
+            expect(listeners[3].fn).toHaveBeenCalled();
+        });
+
+        it('can remove all listeners at once', function () {
+            // prepare
+            this.oculus.off();
+            // execute
+            events.forEach(this.oculus.trigger, this.oculus);
+            // verify
+            expect(listeners[0].fn).not.toHaveBeenCalled();
+            expect(listeners[1].fn).not.toHaveBeenCalled();
+            expect(listeners[2].fn).not.toHaveBeenCalled();
+            expect(listeners[3].fn).not.toHaveBeenCalled();
+            expect(this.oculus.events).toEqual({});
         });
     });
 });
