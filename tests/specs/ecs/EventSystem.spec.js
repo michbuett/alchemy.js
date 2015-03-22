c4eb1b36cc42c5a29ebb6ce9e2af475f26e627a5
Michael Buettner <michbuett@gmx.de>
2015-03-22 20:42:51 +0100

    Implement component system for events

--- tests/specs/ecs/EventSystem.spec.js
+++ tests/specs/ecs/EventSystem.spec.js
@@ -1,29 +1,67 @@
-describe('alchemy.web.HierarchySystem', function () {
+describe('alchemy.ecs.EventSystem', function () {
     'use strict';
 
     var alchemy = require('./../../../lib/core/Alchemy.js');
 
     beforeEach(function () {
-        setFixtures(sandbox());
-
-        this.apothecarius = alchemy('alchemy.ecs.Apothecarius').brew();
-
-        initEntities(this.apothecarius);
+        setFixtures('<div id="foo"></div>');
     });
 
-    it('allows to delegate event handler', function () {
+    it('allows to delegate DOM events to application messages', function () {
         // prepare
+        var delegatus = alchemy('alchemy.web.Delegatus').brew();
+        var apothecarius = initEntities();
         var testSubject = alchemy('alchemy.ecs.EventSystem').brew({
-            entites: this.apothecarius
+            delegator: delegatus,
+            entities: apothecarius,
         });
+        testSubject.addHandler('fooClick', function () {});
 
         // execute
         testSubject.update();
 
         // verify
+        expect(apothecarius.getComponent('foo', 'events')).toBeFalsy();
+        expect(apothecarius.getComponent('foo', 'delegatedEvents').current.val()).toEqual([{
+            event: 'click',
+            delegate: 0,
+        }]);
     });
 
+    it('allows to register custom handler functions when defining entity types', function () {
+        // prepare
+        var testSubject = alchemy('alchemy.ecs.EventSystem').brew();
+        var entityDescriptor = {
+            getEventHandler: function () {
+            },
+        };
+        spyOn(entityDescriptor, 'getEventHandler').andCallThrough();
 
-    function initEntities(apothecarius) {
+        // execute
+        testSubject.defineEntity(entityDescriptor);
+
+        // verify
+        expect(entityDescriptor.getEventHandler).toHaveBeenCalled();
+    });
+
+    it('ignores entity descriptors which do not provide new handle', function () {
+        expect(function () {
+            var testSubject = alchemy('alchemy.ecs.EventSystem').brew();
+            testSubject.defineEntity({});
+        }).not.toThrow();
+    });
+
+    function initEntities() {
+        var apothecarius = alchemy('alchemy.ecs.Apothecarius').brew();
+        apothecarius.createEntity({
+            id: 'foo',
+            events: {
+                listener: [{
+                    event: 'click',
+                    handler: 'fooClick',
+                }]
+            },
+        });
+        return apothecarius;
     }
 });
