b688b90b59e0dc8ff1427571614111a63e5ce744
michbuett <michbuett@gmx.de>
2016-09-29 20:09:59 +0200

    Added alternative CSS render system

---
+++ tests/specs/ecs/CssRenderSystem.spec.js
@@ -0,0 +1,99 @@
+/* global $ */
+describe('alchemy.lib.CssRenderSystem', function () {
+    'use strict';
+
+    var Stylus = require('./../../../lib/Stylus');
+    var CssRenderSystem = require('./../../../lib/CssRenderSystem');
+
+    beforeEach(function () {
+        this.stylus = Stylus.brew();
+
+        setFixtures([
+            '<div id="foo">FOO',
+                '<div id="bar">BAR',
+                    '<div id=baz>BAZ</div>',
+                '</div>',
+            '</div>',
+        ].join());
+    });
+
+    afterEach(function () {
+        this.stylus.dispose();
+        this.stylus = null;
+    });
+
+    describe('update', function () {
+        it('allows to render entity specific css', function () {
+            // prepare
+            var entities = new Map([
+                ['foo', {
+                    css: {
+                        '#foo': {
+                            'color': '#00FF00',
+                            'background-color': '#FF00FF',
+                        }
+                    }
+                }],
+                ['bar', {
+                    css: {
+                        '#bar': {
+                            'color': '#00FFFF',
+                            'background-color': '#00FF00',
+                        },
+                    }
+                }]
+            ]);
+
+            var testSubject = CssRenderSystem.brew({
+                stylus: this.stylus,
+            });
+
+            // execute
+            testSubject.update(entities);
+
+            // verify
+            expect($('div#foo')).toHaveCss({
+                'color': 'rgb(0, 255, 0)',
+                'background-color': 'rgb(255, 0, 255)'
+            });
+            expect($('div#bar')).toHaveCss({
+                'color': 'rgb(0, 255, 255)',
+                'background-color': 'rgb(0, 255, 0)'
+            });
+        });
+
+        it('ignores entities without a valid css component', function () {
+            // prepare
+            var entities = new Map([
+                ['foo', { id: 'foo', css: null, }],
+                ['bar', { id: 'bar', css: 'none', }]
+            ]);
+
+            var testSubject = CssRenderSystem.brew({
+                stylus: this.stylus,
+            });
+
+            // execute
+            expect(function () {
+                testSubject.update(entities);
+
+            // verify
+            }).not.toThrow();
+        });
+    });
+
+    describe('dispose', function () {
+        it('removes the reference to the apothecarius', function () {
+            // prepare
+            var testSubject = CssRenderSystem.brew({
+                stylus: this.stylus,
+            });
+
+            // execute
+            testSubject.dispose();
+
+            // verify
+            expect(testSubject.stylus).toBeFalsy();
+        });
+    });
+});
