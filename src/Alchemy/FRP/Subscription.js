5a8cb2ab626d89652b1f4dceef689b528746aa76
michbuett <michbuett@gmx.de>
2018-06-08 15:46:49 +0200

    Removing obsolete files (experimental relicts)

--- src/Alchemy/FRP/Subscription.js
+++ src/Alchemy/FRP/Subscription.js
@@ -5,29 +5,9 @@ exports.together = function (consumerArr) {
     })
 
     return function () {
-      // console.trace()
       unsubs.forEach(function (unsub) {
         unsub()
       })
-    }
-  }
-}
-
-exports.switcher = function (createSink) {
-  return function (event) {
-    return function (initialVal) {
-      return function () {
-        var unsubS = createSink(initialVal)()
-        var unsubE = event.subscribe(function (val) {
-          unsubS()
-          unsubS = createSink(val)()
-        })
-
-        return function () {
-          unsubE()
-          unsubS()
-        }
-      }
     }
   }
 }
